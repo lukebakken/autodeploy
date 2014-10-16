e985b77cb3011ed528bf37ec63d5d2355eddf003
Luke Bakken <lbakken@basho.com>
2014-10-16 16:04:48 +0000

    nuke erlexec for now and use sudo

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 PROJECT = autodeploy
-DEPS = cowboy lager erlexec
-dep_erlexec = git git://github.com/saleyn/erlexec.git
+DEPS = cowboy lager
 ERLC_OPTS += +'{parse_transform, lager_transform}'
 TEST_ERLC_OPTS += +'{parse_transform, lager_transform}'
 include erlang.mk
--- src/git_util.erl
+++ src/git_util.erl
@@ -4,11 +4,12 @@
 
 clone(GitCloneUrl, {GitRepoPath, GitRepoUser, GitRepoGroup}) ->
     {ok, TmpFile} = get_tmp_file(),
-    ok = build_script(TmpFile, GitCloneUrl, GitRepoPath),
+    ok = build_script(TmpFile, GitCloneUrl, GitRepoPath, GitRepoUser, GitRepoGroup),
     % TODO: add -x when debug/test
     % TODO: how best to globally indicate debug vs production
-    % NB: must use "sudo" as erlexec's run as user/group is broken
-    {ok, RunResult} = exec:run(["/usr/bin/sudo", "-u", GitRepoUser, "-g", GitRepoGroup, "/bin/sh", TmpFile], [sync, stdout]),
+    GitCmd = ["/usr/bin/sudo", "-u", GitRepoUser, "-g", GitRepoGroup, "/bin/sh", TmpFile],
+    lager:debug("git cmd: ~p", [GitCmd]),
+    RunResult = os:cmd(string:join(GitCmd, " ")),
     lager:debug("git result: ~p", [RunResult]),
     ok = file:delete(TmpFile),
     {ok, RunResult}.
@@ -19,15 +20,14 @@ get_tmp_file() ->
     TmpFile = filename:join(TmpDir, lists:concat([TmpName, ".tmp"])),
     {ok, TmpFile}.
 
-build_script(TmpFile, GitCloneUrl, GitRepoPath) ->
+build_script(TmpFile, GitCloneUrl, GitRepoPath, GitRepoUser, GitRepoGroup) ->
     {ok, GitPath} = application:get_env(autodeploy, git),
     {ok, F} = file:open(TmpFile, [write]),
     ok = file:write(F, "#!/bin/sh\n"),
     ok = file:write(F, "set -o errexit\n"),
     ok = file:write(F, "id -a\n"),
-    ok = io:format(F, "echo 'rm -rf ~s > /tmp/clone.out 2>&1'~n", [GitRepoPath]),
-    ok = io:format(F, "rm -rf ~s > /tmp/clone.out 2>&1~n", [GitRepoPath]),
-    ok = io:format(F, "echo '~s clone --verbose --progress ~s ~s > /tmp/clone.out 2>&1'~n", [GitPath, GitCloneUrl, GitRepoPath]),
-    ok = io:format(F, "~s clone --verbose --progress ~s ~s >> /tmp/clone.out 2>&1~n", [GitPath, GitCloneUrl, GitRepoPath]),
+    ok = io:format(F, "find ~s -delete~n", [GitRepoPath]),
+    ok = io:format(F, "~s clone --verbose --progress ~s ~s > /tmp/clone.out 2>&1~n", [GitPath, GitCloneUrl, GitRepoPath]),
+    %% ok = io:format(F, "/bin/chown -vR ~s:~s ~s>> /tmp/clone.out 2>&1~n", [GitRepoUser, GitRepoGroup, GitRepoPath]),
     ok = file:write(F, "exit 0\n"),
     ok = file:close(F).
