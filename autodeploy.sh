654cacdfe869f42b718a1ba26b604ab9e857f3e0
Luke Bakken <luke@bowbak.org>
2014-12-22 20:13:58 -0800

    separate clone path from bin dir

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+rel_output_dir='../autodeploy'
 backup_dir='../cfgbak'
-sys_config='./_rel/autodeploy_release/releases/1/sys.config'
+sys_config="$rel_output_dir/autodeploy_release/releases/1/sys.config"
 mkdir "$backup_dir"
 set -o errexit
 case "$1" in
@@ -13,7 +14,7 @@ case "$1" in
     ;;
   'post')
     source /home/erlang/installs/17.4/activate
-    make clean release
+    make RELX_OUTPUT_DIR="$rel_output_dir" clean release
     ;;
   *)
     echo '[error] argument must be "pre" or "post"'
