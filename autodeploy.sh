c0c71d42e0c96fcacc132ea73736770dab54bea2
Luke Bakken <luke@bowbak.org>
2014-12-22 19:55:34 -0800

    tweak autodeploy script

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-backup_dir=../cfgbak
+backup_dir='../cfgbak'
+sys_config='./_rel/autodeploy_release/releases/1/sys.config'
 mkdir "$backup_dir"
 set -o errexit
 case "$1" in
   'pre')
     now="$(date '+%Y%m%d_%H%M%S')"
-    mv -vf ./_rel/autodeploy_release/releases/1/sys.config date "$backup_dir/sys-config-${now}.bak"
+    if [[ -f $sys_config ]]
+    then
+      mv -vf "$sys_config" "$backup_dir/sys-config-${now}.bak"
+    fi
     ;;
   'post')
     source /home/erlang/installs/17.4/activate
